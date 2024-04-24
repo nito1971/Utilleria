fd38ce0bc984b46f0c3870059cdd9a86ef236b6f
nito1971 <bgonpin@gmail.com>
2024-04-24 22:34:15 +0200

    Correcciones.

--- buscar_con_control_excepciones.py
+++ buscar_con_control_excepciones.py
@@ -4,6 +4,18 @@ import re
 
 carpeta_busqueda = "/mnt/local/datos/Contras/Collection 1"
 
+def abrir_otros(ruta_completa, patron):
+    try:
+        with open(os.path.join(ruta_completa), "r", encoding="latin1") as archivo_normal:
+            for num_linea, linea in enumerate(archivo_normal, start=1):
+                if re.search(re.escape(patron), linea):  # Escape patron input
+                    print(f"Coincidencia encontrada en {ruta_completa}, línea {num_linea}: {linea.strip()}")
+    except exit as e:
+        print(f"Error al procesar archivo normal: {e}")
+        pass
+ 
+
+# Buscar en archivos comprimidos
 def abrir_gz(ruta_completa, patron):
     try:
         with gzip.open(ruta_completa, "rt", encoding="latin1") as archivo_comprimido:
@@ -15,7 +27,7 @@ def abrir_gz(ruta_completa, patron):
         pass
 
 
-def buscar_en_archivos_comprimidos(carpeta, patron):
+def semilla(carpeta, patron):
     """
     Busca el patrón en todos los archivos comprimidos (.gz) dentro de la carpeta especificada.
     :param carpeta: Ruta de la carpeta donde se encuentran los archivos comprimidos.
@@ -23,10 +35,11 @@ def buscar_en_archivos_comprimidos(carpeta, patron):
     """
     try:
         for archivo in os.listdir(carpeta):
-            if archivo.endswith(".gz"):                
-                ruta_completa = os.path.join(carpeta, archivo)
+            ruta_completa = os.path.join(carpeta, archivo)
+            if archivo.endswith(".gz"):               
                 abrir_gz(ruta_completa, patron)
             else:
+                abrir_otros(ruta_completa, patron)
                 with open(os.path.join(carpeta, archivo), "r", encoding="latin1") as archivo_normal:
                     for num_linea, linea in enumerate(archivo_normal, start=1):
                         if re.search(re.escape(patron), linea):  # Escape patron input
@@ -49,7 +62,7 @@ def main():
         if opcion == "1":
             carpeta_busqueda = input("Ingrese la ruta de la carpeta a buscar: ")
             patron_busqueda = input("Ingrese el patrón a buscar: ")            
-            buscar_en_archivos_comprimidos(carpeta_busqueda, patron_busqueda)
+            semilla(carpeta_busqueda, patron_busqueda)
         elif opcion == "2":
             print("Adiós!")
             break
